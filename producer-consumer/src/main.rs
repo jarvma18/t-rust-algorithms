ad0769bcebfe1d5346aa7e298bc229bfa2bb6196
Markus Järvinen <markusmjarvinen@outlook.com>
2026-06-16 00:19:50 +0300

    Added some initial threading to producer-consumer main.rs, goal next is to create program which has shared fixed buffer and logic to manage the producer and consumer inconsistencies.

--- producer-consumer/src/main.rs
+++ producer-consumer/src/main.rs
@@ -1,3 +1,26 @@
+use std::thread;
+use std::time::Duration;
+
+fn producer() {
+    println!("Hello, I am a producer!");
+}
+
+fn consumer() {
+    println!("Hello, I am a consumer!");
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, I will start both producer and consumer tasks!");
+
+    thread::spawn(|| {
+        for _i in 1..10 {
+            producer();
+            thread::sleep(Duration::from_millis(100));
+        }
+    });
+
+    for _i in 1..10 {
+        consumer();
+        thread::sleep(Duration::from_millis(200));
+    }
 }
