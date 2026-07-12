1304e8c8d6ff80894c16be2ba0928cb38e485acd
Markus Järvinen <markusmjarvinen@outlook.com>
2026-07-13 01:32:00 +0300

    Added Mutex and Arcs to handle sharing Vec typed variable between different threads, this is not ideal right now, but doing this just for the learning purposes only.

--- producer-consumer/src/main.rs
+++ producer-consumer/src/main.rs
@@ -1,26 +1,50 @@
+use std::sync::{Mutex, Arc};
 use std::thread;
 use std::time::Duration;
 
-fn producer() {
-    println!("Hello, I am a producer!");
-}
-
-fn consumer() {
-    println!("Hello, I am a consumer!");
-}
-
 fn main() {
-    println!("Hello, I will start both producer and consumer tasks!");
+    let buffer = Arc::new(Mutex::new(Vec::<i32>::new()));
+    println!("Starting producers and consumers..");
 
-    thread::spawn(|| {
-        for _i in 1..10 {
-            producer();
+    let producer = {
+        let producer_buffer = Arc::clone(&buffer);
+        thread::spawn(move || {
+            loop {
+                producer_buffer.lock().unwrap().push(42);
+                thread::sleep(Duration::from_millis(150));
+            }
+        })
+    };
+
+    // Printer
+    let printer_buffer = Arc::clone(&buffer);
+    thread::spawn(move || {
+        loop {
+            {
+                let locked_printer_buffer = printer_buffer.lock().unwrap();
+                println!("{:?}", locked_printer_buffer);
+            }
             thread::sleep(Duration::from_millis(100));
         }
     });
 
-    for _i in 1..10 {
-        consumer();
-        thread::sleep(Duration::from_millis(200));
-    }
+    let consumer = {
+        let consumer_buffer = Arc::clone(&buffer);
+        thread::spawn(move || {
+            loop {
+                {
+                    let mut locked_consumer_buffer = consumer_buffer.lock().unwrap();
+                    if locked_consumer_buffer.is_empty() {
+                        println!("Buffer is empty");
+                    }
+                    else {
+                        locked_consumer_buffer.remove(0);
+                    }
+                }
+                thread::sleep(Duration::from_millis(200));
+            }
+        })
+    };
+    producer.join().unwrap();
+    consumer.join().unwrap();
 }
