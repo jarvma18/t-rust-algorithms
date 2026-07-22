31d29edd730c88767137dae9138dbad2806756ed
Markus Järvinen <markusmjarvinen@outlook.com>
2026-07-23 00:33:07 +0300

    Refactored the first implementation of producer-consumer problem to follow better pattern:
    * Imported Condvar to handle waiting for acting inside different thread e.g. waiting for consumable or producable event vs. polling continuously
    * Extracted the data to own inner struct and implementing that one inside BoundedBuffer in order to use Mutex to it
    * Moved the whole buffer into struct with own methods to hide the implementation details of it e.g. needing to check things inside the thread rather than now calling .produce or .consume which handles that check
    * Removed printer thread and just printing stuff in produce or consume functions
    * Changed if conditionals to while since that is better pattern to handle those checks

--- producer-consumer/src/main.rs
+++ producer-consumer/src/main.rs
@@ -1,56 +1,86 @@
-use std::sync::{Mutex, Arc};
+use std::sync::{Mutex, Arc, Condvar};
 use std::thread;
 use std::time::Duration;
 use std::collections::VecDeque;
 
+struct Inner {
+  data: VecDeque<i32>
+}
+
+struct BoundedBuffer {
+  inner: Mutex<Inner>,
+  capacity: usize,
+  can_produce: Condvar,
+  can_consume: Condvar
+}
+
+impl BoundedBuffer {
+  pub fn new(capacity: usize) -> Self {
+    Self {
+      inner: Mutex::new(Inner {
+        data: VecDeque::with_capacity(capacity),
+      }),
+      capacity,
+      can_produce: Condvar::new(),
+      can_consume: Condvar::new()
+    }
+  }
+
+  pub fn produce(&self, item: i32) {
+    let mut inner = self.inner.lock().unwrap();
+    while inner.data.len() == self.capacity {
+      println!("Buffer is full, cannot produce anymore items to it");
+      inner = self.can_produce.wait(inner).unwrap();
+    }
+    inner.data.push_back(item);
+    println!("Produced: {}. Current size: {}", item, inner.data.len());
+    self.can_consume.notify_one();
+  }
+
+  pub fn consume(&self) {
+    let mut inner = self.inner.lock().unwrap();
+    while inner.data.is_empty() {
+      println!("Buffer is empty, cannot consume anymore items from it");
+      inner = self.can_consume.wait(inner).unwrap();
+    }
+    let item = inner.data.pop_front().unwrap();
+    println!("Consumed: {}", item);
+    self.can_produce.notify_one();
+  }
+}
+
 fn main() {
-  let buffer = Arc::new(Mutex::new(VecDeque::<i32>::with_capacity(5)));
-  println!("Starting producers and consumers..");
+  const CAPACITY: usize = 5;
+
+  println!("\n");
+  println!("##########################################");
+  println!("Starting producer-consumer problem!");
+  println!("stop by pressing CTRL + C");
+  println!("##########################################");
+  println!("\n");
+
+  let buffer = Arc::new(BoundedBuffer::new(CAPACITY));
+  let producer_buffer = Arc::clone(&buffer);
+  let consumer_buffer = Arc::clone(&buffer);
 
   let producer = {
-    let producer_buffer = Arc::clone(&buffer);
     thread::spawn(move || {
       loop {
         {
-          let mut locked_producer_buffer = producer_buffer.lock().unwrap();
-          if locked_producer_buffer.len() < 5 {
-            locked_producer_buffer.push_front(42);
-          }
-          else {
-            println!("Buffer is full");
-          }
+          producer_buffer.produce(42);
         }
-        thread::sleep(Duration::from_millis(99));
+        thread::sleep(Duration::from_millis(1000));
       }
     })
   };
 
-  // Printer
-  let printer_buffer = Arc::clone(&buffer);
-  thread::spawn(move || {
-    loop {
-      {
-        let locked_printer_buffer = printer_buffer.lock().unwrap();
-        println!("{:?}", locked_printer_buffer);
-      }
-      thread::sleep(Duration::from_millis(100));
-    }
-  });
-
   let consumer = {
-    let consumer_buffer = Arc::clone(&buffer);
     thread::spawn(move || {
       loop {
         {
-          let mut locked_consumer_buffer = consumer_buffer.lock().unwrap();
-          if locked_consumer_buffer.is_empty() {
-            println!("Buffer is empty");
-          }
-          else {
-            locked_consumer_buffer.pop_back();
-          }
+          consumer_buffer.consume();
         }
-        thread::sleep(Duration::from_millis(100));
+        thread::sleep(Duration::from_millis(1000));
       }
     })
   };
